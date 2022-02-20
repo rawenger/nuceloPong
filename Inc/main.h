1ff605588ff7b08e4b710ed0dfe29b7353274e78
Ryan Wenger <ryanwenger@umail.ucsb.edu>
2022-02-20 14:54:07 -0800

    initial project sources

---
+++ Inc/main.h
@@ -0,0 +1,15 @@
+/* main.h */
+
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
---
+++ Src/main.c
@@ -0,0 +1,14 @@
+/* nucleoPong -- A USB-based bot for the iOS GamePigeon Cup Pong game
+ */
+
+/*
+ * Copyright (c) 2022 Ryan Wenger.
+ * This file is provided AS-IS with no warranty.
+ */
+
+#include "main.h"
+
+int main(void) {
+
+    return 0;
+}
